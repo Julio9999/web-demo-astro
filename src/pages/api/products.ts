a9b7e7f9b4d440b4922f69023a629180493b7649
Julio Espinola <90486971+Julio9999@users.noreply.github.com>
2025-10-29 20:53:36 -0300

    cambio

--- src/pages/api/products.ts
+++ src/pages/api/products.ts
@@ -2,7 +2,7 @@
 export const prerender = false;
 import { db, Product } from "astro:db";
 
-export async function get() {
+export async function GET() {
   const products = await db
   .select({ id: Product.id, name: Product.name, price: Product.price })
   .from(Product)
